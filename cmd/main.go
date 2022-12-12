753d722d11805bee89341f1d77730458ab13e131
OTengnér <olle.tengner@sudosweden.com>
2022-12-12 14:27:02 +0100

    Everything is up except the secret file

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	"Backend/internal"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	internal.LoadEnvVariables()
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World, Im Alive",
+		})
+	})
+
+	r.Run()
+}
