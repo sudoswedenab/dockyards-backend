11002570628b5b042f270238048ad88a59581c88
OTengnér <olle.tengner@sudosweden.com>
2022-12-13 11:54:58 +0100

    fix

--- api/v1/routes/routes.go
+++ api/v1/routes/routes.go
@@ -4,8 +4,17 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(r *gin.Engine) {
 
+	///http://localhost:9000/api
 	routes := r.Group("/")
 	routes.GET("/api", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World, Im Alive",
+		})
+	})
+
+	///http://localhost:9000/v1/login//
+	apione := r.Group("/v1")
+	apione.GET("/login", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World, Im Alive",
 		})
