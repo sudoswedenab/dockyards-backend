96b521d0fd4f3b8ba52c4993f8f3cf718b489167
OTengnér <olle.tengner@sudosweden.com>
2022-12-14 11:50:46 +0100

    logic for login

--- api/v1/handlers/login.go
+++ api/v1/handlers/login.go
@@ -2,4 +2,15 @@ package handlers
 
 func Login() {
 	println("Login hit")
+
+	// Get email and pass off req body
+
+	//Look up requested User
+
+	//Compare sent in pass with saved user pass hash
+
+	//Generate a jwt token
+
+	//send it back
+
 }
