c61cad0a2af631c87006e8325b231f46272c8729
OTengnér <olle.tengner@sudosweden.com>
2022-12-15 12:42:42 +0100

    fixed

--- api/v1/handlers/validate.go
+++ api/v1/handlers/validate.go
@@ -9,6 +9,11 @@ import (
 func Validate(c *gin.Context) {
 	println("AUTH hit")
 
+	c.JSON(http.StatusOK, gin.H{
+		"hey": "user logged in",
+	})
+
+	// Get out info about user:
 	user, _ := c.Get("user")
 
 	c.JSON(http.StatusOK, gin.H{
