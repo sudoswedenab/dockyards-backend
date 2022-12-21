a50f967d91314297560e043bc10679aa0e60980e
OTengnér <olle.tengner@sudosweden.com>
2022-12-21 10:17:07 +0100

    ..

--- api/v1/handlers/Jwt/refreshEndpoint.go
+++ api/v1/handlers/Jwt/refreshEndpoint.go
@@ -1,8 +1,12 @@
 package jwt
 
 import (
+	"Backend/api/v1/models"
+	"Backend/internal"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
@@ -13,29 +17,37 @@ import (
 // https://godoc.org/github.com/dgrijalva/jwt-go#example-Parse--Hmac
 
 func RefreshTokenEndpoint(c *gin.Context) error {
-	type tokenReqBody struct {
-		RefreshToken string `json:"refresh_token"`
-	}
-	tokenReq := tokenReqBody{}
-	c.Bind(&tokenReq)
 
+	//Get the cookie
+	refreshToken, err := c.Cookie("refreshtoken")
+
+	if err != nil {
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
 	// Parse takes the token string and a function for looking up the key.
 	// The latter is especially useful if you use multiple keys for your application.
 	// The standard is to use 'kid' in the head of the token to identify
 	// which key to use, but the parsed token (head and claims) is provided
 	// to the callback, providing flexibility.
-	token, err := jwt.Parse(tokenReq.RefreshToken, func(token *jwt.Token) (interface{}, error) {
+
+	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return []byte("secret"), nil
+		return []byte(os.Getenv("RefSECERET")), nil
 	})
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		// Get the user record from database or
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
 
+		//Find the user with token sub
+		var user models.User
+		internal.DB.First(&user, claims["sub"])
 		// run through your business logic to verify if the user can log in
 		if int(claims["sub"].(float64)) == 1 {
 
