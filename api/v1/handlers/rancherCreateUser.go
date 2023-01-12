a0da7652c01089afa13294769caca8e28f75a3fc
Adam Theodor Högberg <theodor.hogberg@sudosweden.com>
2023-01-12 17:18:40 +0100

    Finish create user rancher

--- api/v1/handlers/rancherCreateUser.go
+++ api/v1/handlers/rancherCreateUser.go
@@ -2,13 +2,21 @@ package handlers
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"go/types"
 	"net/http"
 	"os"
 )
+
+type RancherUser struct {
+	Enabled            bool   `json:"enabled"`
+	MustChangePassword bool   `json:"mustChangePassword"`
+	Name               string `json:"name"`
+	Password           string `json:"password"`
+	Username           string `json:"username"`
+}
 
 // RancherCreateUser godoc
 //
@@ -18,23 +26,15 @@ import (
 //	@Success		200
 //	@Router			/create-user [post]
 func RancherCreateUser(c *gin.Context) {
-	var body struct {
-		enabled            bool
-		mustChangePassword bool
-		name               string
-		password           string
-		principalIds       types.Array
-		username           string
-	}
-
-	if c.Bind(&body) != nil {
+	user := RancherUser{}
+	if c.Bind(&user) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to read Body",
 		})
 		return
 	}
 
-	reqBody, err := json.Marshal(body)
+	reqBody, err := json.Marshal(user)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to marshal Body",
@@ -42,17 +42,18 @@ func RancherCreateUser(c *gin.Context) {
 		return
 	}
 
-	accessKey := os.Getenv("CATTLE_ACCESS_KEY")
-	secretKey := os.Getenv("CATTLE_SECRET_KEY")
-	rancherUrl := os.Getenv("CATTLE_URL")
+	bearerToken := os.Getenv("CATTLE_BEARER_TOKEN")
+	rancherURL := os.Getenv("CATTLE_URL")
 
+	// Do external request
 	client := &http.Client{}
-	req, _ := http.NewRequest("POST", rancherUrl+"/v3/users", bytes.NewBuffer(reqBody))
-	req.Header.Add("Authorization", fmt.Sprintf("%s:%s", accessKey, secretKey)) //base64?
+	req, _ := http.NewRequest("POST", rancherURL+"/v3/users", bytes.NewBuffer(reqBody))
+	req.Header.Set(
+		"Authorization", "Basic "+b64.StdEncoding.EncodeToString([]byte(bearerToken)),
+	)
 	resp, _ := client.Do(req)
 
 	if resp.Status == "201" {
-		fmt.Println("we happy noe.")
+		fmt.Println("be happy.")
 	}
-
 }
