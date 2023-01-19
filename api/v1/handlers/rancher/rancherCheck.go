db0cf5bbe5a1c41944987f5b47cfeba6087a2f54
OTengnér <olle.tengner@sudosweden.com>
2023-01-20 00:22:30 +0100

    seems to work now

--- api/v1/handlers/rancher/rancherCheck.go
+++ api/v1/handlers/rancher/rancherCheck.go
@@ -2,25 +2,25 @@ package rancher
 
 import (
 	"Backend/api/v1/model"
+	"errors"
 	"fmt"
 )
 
-func RancherCheck(user model.User) string {
+func RancherCheck(user model.User) (string, error) {
 
 	NewRanchPWd := ChangeRancherPWD(user)
 	RancherBearerToken, RancherUserID := CreateRancherToken(model.RRtoken{Name: user.Name, Password: NewRanchPWd})
 	fmt.Println(RancherBearerToken)
 
 	if RancherUserID != user.RancherID {
+
 		// c.JSON(http.StatusBadRequest, gin.H{
 		// 	"error": "Invalid email",
-		return ""
+		err := errors.New("invalid email or password")
+		return "", err
 	}
 	// fmt.Println("DBUSER", user.RancherID)
 	// fmt.Println(RancherUserID)
 
-	// c.JSON(http.StatusOK, gin.H{
-	// 	"UserStatus": "user logged in",
-	// })
-	return RancherBearerToken
+	return RancherBearerToken, nil
 }
