485fa4863f824841b8b3cb0182557aa0b0413e8f
OTengnér <olle.tengner@sudosweden.com>
2023-01-20 01:08:03 +0100

    update

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,12 @@ import (
 	"Backend/api/v1/routes"
 	_ "Backend/docs"
 	"Backend/internal"
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"time"
 )
 
 func init() {
