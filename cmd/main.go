cf609f48c61dec1402e20113ef955d50898bc83f
OTengnér <olle.tengner@sudosweden.com>
2023-01-20 01:08:03 +0100

    update

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,12 @@ import (
 	"Backend/api/v1/routes"
 	_ "Backend/docs"
 	"Backend/internal"
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"time"
 )
 
 func init() {
@@ -34,19 +35,19 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	routes.RegisterRoutes(r)
-
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000/"},
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"POST", "PUT", "GET", "DELETE"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Origin, Content-Type"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "http://localhost:3000/"
+			return origin == "http://localhost:3000"
 		},
 		MaxAge: 12 * time.Hour,
 	}))
+
+	routes.RegisterRoutes(r)
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
