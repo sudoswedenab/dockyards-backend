731d83d9bb60fa9f99dc2b6f656ab2a7c8a06384
Adam Theodor Högberg <theodor.hogberg@sudosweden.com>
2023-01-26 09:46:21 +0100

    Fix waitgroup

--- internal/connectToDb.go
+++ internal/connectToDb.go
@@ -12,8 +12,7 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB(wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("trying to connect")
+	fmt.Println("Trying to connect..")
 	dsn := os.Getenv("DB")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	DB = db
@@ -21,6 +20,9 @@ func ConnectToDB(wg *sync.WaitGroup) {
 		fmt.Println("Failed to connect to database, trying again..")
 		time.Sleep(time.Second * 3)
 		ConnectToDB(wg)
+	} else {
+		fmt.Println("Success!")
+		wg.Done()
 	}
 	return
 }
