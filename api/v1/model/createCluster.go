2c457cbfe3fa5dd413729b8b32aa4815628db060
OTengnér <olle.tengner@sudosweden.com>
2023-02-03 13:11:08 +0100

    adding struct

---
+++ api/v1/model/createCluster.go
@@ -0,0 +1,19 @@
+package model
+
+type ClusterData struct {
+	DockerRootDir             string           `json:"dockerRootDir"`
+	Type                      string           `json:"type"`
+	Name                      string           `json:"name"`
+	ClusterTemplateRevisionId string           `json:"clusterTemplateRevisionId"`
+	ClusterTemplateId         string           `json:"clusterTemplateId"`
+	ClusterSecrets            []clusterSecrets `json:"clusterSecrets"`
+}
+
+type clusterSecrets struct {
+	Type    string    `json:"type"`
+	Answers []answers `json:"answers"`
+}
+
+type answers struct {
+	Values map[string]string `json:"values"`
+}
