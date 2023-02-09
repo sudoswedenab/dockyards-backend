6e86a00195a5a4e195f5b7912705195b5f468625
OTengnér <olle.tengner@sudosweden.com>
2023-02-09 11:14:08 +0100

    fix code

--- api/v1/middleware/requireAuth.go
+++ api/v1/middleware/requireAuth.go
@@ -24,7 +24,7 @@ func RequireAuth(c *gin.Context) {
 	// Parse takes the token string and a function for looking up the key.
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
