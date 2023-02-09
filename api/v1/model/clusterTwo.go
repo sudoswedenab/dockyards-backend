07d270dc8cd3905c2b531d00625d1614bd5018cd
OTengnér <olle.tengner@sudosweden.com>
2023-02-09 12:31:22 +0100

    changes

--- api/v1/model/clusterTwo.go
+++ api/v1/model/clusterTwo.go
@@ -12,14 +12,30 @@ package model
 //  "nodeTemplateId": "cattle-global-nt:nt-zd2tl"}
 
 type NodePoolbody struct {
-	ControlPlane            bool   `json:"controlPlane"`
-	DeleteNotReadyAfterSecs int    `json:"deleteNotReadyAfterSecs"`
-	DrainBeforeDelete       bool   `json:"drainBeforeDelete"`
-	Etcd                    bool   `json:"etcd"`
-	Quantity                int    `json:"quantity"`
-	Worker                  bool   `json:"worker"`
-	Type                    string `json:"nodePool"`
-	ClusterId               string `json:"clusterId"`
-	HostnamePrefix          string `json:"hostnamePrefix"`
-	NodeTemplateId          string `json:"nodeTemplateId"`
+	ControlPlane            bool     `json:"controlPlane"`
+	DeleteNotReadyAfterSecs int      `json:"deleteNotReadyAfterSecs"`
+	DrainBeforeDelete       bool     `json:"drainBeforeDelete"`
+	Etcd                    bool     `json:"etcd"`
+	Quantity                int      `json:"quantity"`
+	Worker                  bool     `json:"worker"`
+	NamespaceId             string   `json:"namespaceId"`
+	Name                    string   `json:"name"`
+	Type                    string   `json:"nodePool"`
+	ClusterId               string   `json:"clusterId"`
+	HostnamePrefix          string   `json:"hostnamePrefix"`
+	NodeTemplateId          string   `json:"nodeTemplateId"`
+	NodeTaints              []string `json:"nodeTaints"`
 }
+
+// clusterId": "c-tvrfj",
+// "controlPlane": true,
+// "deleteNotReadyAfterSecs": 0,
+// "drainBeforeDelete": true,
+// "etcd": true,
+// "hostnamePrefix": "kappastwo-node-",
+// "name": "",
+// "namespaceId": "",
+// "nodeTaints": [ ],
+// "nodeTemplateId": "cattle-global-nt:nt-zd2tl",
+// "quantity": 3,
+// "worker": true
