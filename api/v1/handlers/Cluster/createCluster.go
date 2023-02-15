291886d59b9a7dd988ebc506ff4c9aed5e090e60
OTengnér <olle.tengner@sudosweden.com>
2023-02-15 16:54:43 +0100

    change orgin against the new URL

--- api/v1/handlers/Cluster/createCluster.go
+++ api/v1/handlers/Cluster/createCluster.go
@@ -78,7 +78,7 @@ func CreatedCluster(c *gin.Context) (string, string, error) {
 		"Content-Type":  {"application/json"},
 		"Authorization": {"Basic " + b64.StdEncoding.EncodeToString([]byte(bearerToken.(string)))},
 		"Accept":        {"application/json"},
-		"Origin":        {"https://ss-di-rancher.sudobash.io"},
+		"Origin":        {"https://ss-di-rancher.mgmt.sudobash.io/"},
 		// "Origin":     {"https://localhost"},
 		"Connection": {"keep-alive"},
 		// "Referer":       {"https://ss-di-rancher.sudobash.io/g/clusters/add/launch/openstack?clusterTemplateRevision=cattle-global-data%3Actr-7xnpl"},
