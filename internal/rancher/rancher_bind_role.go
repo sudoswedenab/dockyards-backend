eaed8d10404de7aec22def12f76fd1a3e7b69551
Alexander Backlund <alexander.backund@sudosweden.com>
2023-03-20 16:51:09 +0100

    Fix bind role in rancher
    Current behaviour is to lookup a role by ID, but the value passed in is
    a role name. This adds a search for the name instead.

--- internal/rancher/rancher_bind_role.go
+++ internal/rancher/rancher_bind_role.go
@@ -3,13 +3,24 @@ package rancher
 import (
 	"fmt"
 
+	"github.com/rancher/norman/types"
 	managementv3 "github.com/rancher/rancher/pkg/client/generated/management/v3"
 )
 
-func (r *Rancher) BindRole(userid, roleid string) error {
-	globalRole, err := r.ManagementClient.GlobalRole.ByID(roleid)
+func (r *Rancher) BindRole(userid, roleName string) error {
+	listOpts := types.ListOpts{
+		Filters: map[string]interface{}{"Name": roleName},
+	}
+	globalRoleCollection, err := r.ManagementClient.GlobalRole.List(&listOpts)
 	if err != nil {
-		return fmt.Errorf("no role named '%s' found", roleid)
+		return err
+	}
+
+	var globalRole managementv3.GlobalRole
+	for _, globalRoleCandidate := range globalRoleCollection.Data {
+		if globalRoleCandidate.Name == roleName {
+			globalRole = globalRoleCandidate
+		}
 	}
 
 	globalRoleBinding := managementv3.GlobalRoleBinding{
