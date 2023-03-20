a99c3c60f1cb98dc9802c1f9644531fc7895fcc5
Adam Theodor Högberg <theodor.hogberg@sudosweden.com>
2023-03-20 12:53:41 +0100

    Update tests for MockRancher and use test for dev rancher cluster

--- internal/rancher/mock/helper.go
+++ internal/rancher/mock/helper.go
@@ -3,19 +3,35 @@ package mock
 import (
 	"bitbucket.org/sudosweden/backend/api/v1/model"
 	"bitbucket.org/sudosweden/backend/internal/rancher"
+	managementv3 "github.com/rancher/rancher/pkg/client/generated/management/v3"
 )
 
 type MockRancherHelper struct {
-	MockRancherCreateUser func(model.RancherUser) (string, error)
-	MockRancherLogin      func(model.User) (string, error)
+	MockRancherCreateUser     func(model.RancherUser) (string, error)
+	MockRancherCreateCluster  func(string, string, string, string) (managementv3.Cluster, error)
+	MockRancherCreateNodePool func(string, string) (managementv3.NodePool, error)
+	MockRancherLogin          func(model.User) (string, error)
+	MockCreateClusterRole     func() error
 }
 
 func (h *MockRancherHelper) RancherCreateUser(user model.RancherUser) (string, error) {
 	return h.MockRancherCreateUser(user)
 }
 
+func (h *MockRancherHelper) RancherCreateCluster(dockerRootDir, name, ctrId, ctId string) (managementv3.Cluster, error) {
+	return h.MockRancherCreateCluster(dockerRootDir, name, ctrId, ctId)
+}
+
+func (h *MockRancherHelper) RancherCreateNodePool(id, name string) (managementv3.NodePool, error) {
+	return h.MockRancherCreateNodePool(id, name)
+}
+
 func (h *MockRancherHelper) RancherLogin(user model.User) (string, error) {
 	return h.MockRancherLogin(user)
+}
+
+func (h *MockRancherHelper) CreateClusterRole() error {
+	return h.MockCreateClusterRole()
 }
 
 var _ rancher.RancherService = &MockRancherHelper{}
--- internal/rancher/rancher_create_user_test.go
+++ internal/rancher/rancher_create_user_test.go
@@ -1,8 +1,7 @@
 package rancher
 
 import (
-	"net/http"
-	"net/http/httptest"
+	"os"
 	"testing"
 
 	"bitbucket.org/sudosweden/backend/api/v1/model"
@@ -12,7 +11,6 @@ func TestRancherCreateUser(t *testing.T) {
 	tt := []struct {
 		name     string
 		user     model.RancherUser
-		handler  func(http.ResponseWriter, *http.Request)
 		expected string
 	}{
 		{
@@ -24,33 +22,28 @@ func TestRancherCreateUser(t *testing.T) {
 				MustChangePassword: false,
 				Enabled:            true,
 			},
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				t.Logf("r: %#v", r)
-				switch r.URL.Path {
-				case "/v3/users":
-					w.WriteHeader(http.StatusCreated)
-					w.Write([]byte(`{"id":"abc123"}`))
-				case "/v3/globalRoles":
-					w.Write([]byte(`{"data":[{"name":"dockyard-role","id":"role123"}]}`))
-				case "/v3/globalrolebindings":
-					w.WriteHeader(http.StatusCreated)
-				case "/":
-					w.Header().Add("X-API-Schemas", r.Host)
-				}
-			},
 			expected: "abc123",
 		},
 	}
 
+	cattleUrl := os.Getenv("INTEGRATION_TEST_CATTLE_URL")
+	bearerToken := os.Getenv("INTEGRATION_TEST_BEARER_TOKEN")
+
+	if cattleUrl == "" {
+		t.Skip("Internal test cattle url not set")
+	}
+
+	if bearerToken == "" {
+		t.Skip("Internal test bearer token not set")
+	}
+
+	r, err := NewRancher(bearerToken, cattleUrl)
+	if err != nil {
+		t.Fatalf("unexpected error creating new rancher: %s", err)
+	}
+
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			ts := httptest.NewServer(http.HandlerFunc(tc.handler))
-
-			r, err := NewRancher("bearer-token", ts.URL)
-			if err != nil {
-				t.Fatalf("unexpected error creating new rancher: %s", err)
-			}
-
 			actual, err := r.RancherCreateUser(tc.user)
 			if err != nil {
 				t.Errorf("expected error to be nil, got %s", err)
@@ -67,15 +60,10 @@ func TestRancherCreateUserErrors(t *testing.T) {
 	tt := []struct {
 		name     string
 		user     model.RancherUser
-		handler  func(http.ResponseWriter, *http.Request)
 		expected string
 	}{
 		{
-			name: "test empty",
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte("empty request"))
-			},
+			name:     "test empty",
 			expected: "unexpected status code 400, data: empty request",
 		},
 		{
@@ -83,52 +71,36 @@ func TestRancherCreateUserErrors(t *testing.T) {
 			user: model.RancherUser{
 				Password: "hello",
 			},
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusBadRequest)
-				w.Write([]byte("password too short"))
-			},
 			expected: "unexpected status code 400, data: password too short",
 		},
 		{
-			name: "test missing dockyard-role",
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				switch r.URL.Path {
-				case "/v3/users":
-					w.WriteHeader(http.StatusCreated)
-					w.Write([]byte(`{"id":"user123"}`))
-				case "/v3/globalRoles":
-					w.Write([]byte(`{"data":[]}`))
-				default:
-					w.WriteHeader(http.StatusTeapot)
-				}
-			},
+			name:     "test missing dockyard-role",
 			expected: "no role named 'dockyard-role' found",
 		},
 		{
-			name: "test role binding internal server error",
-			handler: func(w http.ResponseWriter, r *http.Request) {
-				switch r.URL.Path {
-				case "/v3/users":
-					w.WriteHeader(http.StatusCreated)
-					w.Write([]byte(`{"id":"user123"}`))
-				case "/v3/globalRoles":
-					w.Write([]byte(`{"data":[{"name":"dockyard-role","id":"role123"}]}`))
-				case "/v3/globalrolebindings":
-					w.WriteHeader(http.StatusInternalServerError)
-					w.Write([]byte("test"))
-				default:
-					w.WriteHeader(http.StatusTeapot)
-				}
-			},
+			name:     "test role binding internal server error",
 			expected: "unexpected status code 500, data: test",
 		},
 	}
 
+	cattleUrl := os.Getenv("INTEGRATION_TEST_CATTLE_URL")
+	bearerToken := os.Getenv("INTEGRATION_TEST_BEARER_TOKEN")
+
+	if cattleUrl == "" {
+		t.Skip("Internal test cattle url not set")
+	}
+
+	if bearerToken == "" {
+		t.Skip("Internal test bearer token not set")
+	}
+
+	r, err := NewRancher(bearerToken, cattleUrl)
+	if err != nil {
+		t.Fatalf("unexpected error creating new rancher: %s", err)
+	}
+
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			ts := httptest.NewServer(http.HandlerFunc(tc.handler))
-
-			r, err := NewRancher("bearer-token", ts.URL)
 			if err != nil {
 				t.Fatalf("unexpected error creating new rancher: %s", err)
 			}
