faa3a304800ea869ad0cc2fff75154e27e22dbff
Alexander Backlund <alexander.backund@sudosweden.com>
2023-04-05 14:20:25 +0200

    Fix typo in node pool options model

--- api/v1/model/model_cluste_container_options.go
+++ api/v1/model/model_cluste_container_options.go
@@ -12,12 +12,12 @@ type Options struct {
 }
 
 type NodePoolOptions struct {
-	ControlPlane              bool   `json:"control_plane"`
-	Etcd                      bool   `json:"etcd"`
-	ControlPlaneComponetsOnly bool   `json:"control_plane_componets_only"`
-	Quantity                  int    `json:"quantity"`
-	Name                      string `json:"name"`
-	CPUCount                  int    `json:"cpu_count"`
-	RAMSize                   int    `json:"ram_size"`
-	DiskSize                  int    `json:"disk_sice"`
+	ControlPlane               bool   `json:"control_plane"`
+	Etcd                       bool   `json:"etcd"`
+	ControlPlaneComponentsOnly bool   `json:"control_plane_components_only"`
+	Quantity                   int    `json:"quantity"`
+	Name                       string `json:"name"`
+	CPUCount                   int    `json:"cpu_count"`
+	RAMSize                    int    `json:"ram_size"`
+	DiskSize                   int    `json:"disk_sice"`
 }
