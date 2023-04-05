967a0f726fdc7fb1dc39a6fc9774891c0ff8f488
Alexander Backlund <alexander.backund@sudosweden.com>
2023-04-05 14:23:44 +0200

    Move node pool options model to dedicated file

--- api/v1/model/model_cluste_container_options.go
+++ api/v1/model/model_cluste_container_options.go
@@ -10,14 +10,3 @@ type Options struct {
 	SingleNode      bool              `json:"single_node"`
 	NodePoolOptions []NodePoolOptions `json:"node_pool_options"`
 }
-
-type NodePoolOptions struct {
-	ControlPlane               bool   `json:"control_plane"`
-	Etcd                       bool   `json:"etcd"`
-	ControlPlaneComponentsOnly bool   `json:"control_plane_components_only"`
-	Quantity                   int    `json:"quantity"`
-	Name                       string `json:"name"`
-	CPUCount                   int    `json:"cpu_count"`
-	RAMSize                    int    `json:"ram_size"`
-	DiskSize                   int    `json:"disk_sice"`
-}
---
+++ api/v1/model/model_node_pool_options.go
@@ -0,0 +1,12 @@
+package model
+
+type NodePoolOptions struct {
+	ControlPlane               bool   `json:"control_plane"`
+	Etcd                       bool   `json:"etcd"`
+	ControlPlaneComponentsOnly bool   `json:"control_plane_components_only"`
+	Quantity                   int    `json:"quantity"`
+	Name                       string `json:"name"`
+	CPUCount                   int    `json:"cpu_count"`
+	RAMSize                    int    `json:"ram_size"`
+	DiskSize                   int    `json:"disk_sice"`
+}
