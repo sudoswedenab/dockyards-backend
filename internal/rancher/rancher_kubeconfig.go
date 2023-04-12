8a836f047458b822fdc8f2fb7f4d00d65c605fcb
Alexander Backlund <alexander.backund@sudosweden.com>
2023-04-12 12:12:01 +0200

    Add get kubeconfig method in rancher package

---
+++ internal/rancher/rancher_kubeconfig.go
@@ -0,0 +1,35 @@
+package rancher
+
+import (
+	"errors"
+
+	"bitbucket.org/sudosweden/backend/api/v1/model"
+	"github.com/rancher/norman/types"
+)
+
+func (r *Rancher) GetKubeConfig(cluster *model.Cluster) (string, error) {
+	listOpts := types.ListOpts{
+		Filters: map[string]interface{}{
+			"name": cluster.Name,
+		},
+	}
+	clusterCollection, err := r.ManagementClient.Cluster.List(&listOpts)
+	if err != nil {
+		return "", err
+	}
+
+	r.Logger.Debug("list cluster collection", "len", len(clusterCollection.Data))
+
+	for _, data := range clusterCollection.Data {
+		if data.Name == cluster.Name {
+			r.Logger.Debug("cluster to generate kubeconfig for found", "id", data.ID, "name", data.Name)
+			generatedKubeConfig, err := r.ManagementClient.Cluster.ActionGenerateKubeconfig(&data)
+			if err != nil {
+				return "", err
+			}
+			return generatedKubeConfig.Config, nil
+		}
+	}
+
+	return "", errors.New("no such cluster")
+}
