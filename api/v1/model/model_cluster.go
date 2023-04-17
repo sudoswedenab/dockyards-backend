20ae36d0ac091db522e3b28e1a3a7033d1eeec03
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-04-17 19:41:45 +0200

    Add organization field to cluster model

--- api/v1/model/model_cluster.go
+++ api/v1/model/model_cluster.go
@@ -3,9 +3,10 @@ package model
 import "time"
 
 type Cluster struct {
-	Name      string    `json:"name"`
-	ID        string    `json:"-"`
-	State     string    `json:"state"`
-	NodeCount int       `json:"node_count"`
-	CreatedAt time.Time `json:"created_at"`
+	Name         string    `json:"name"`
+	ID           string    `json:"-"`
+	State        string    `json:"state"`
+	NodeCount    int       `json:"node_count"`
+	CreatedAt    time.Time `json:"created_at"`
+	Organization string    `json:"org,omitempty"`
 }
