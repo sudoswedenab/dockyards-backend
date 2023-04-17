79fd07f4b558b631985ce89e69914353613cdc8c
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-04-17 14:31:41 +0200

    Add organizations reference to user model
    To be able to fetch organizations that a user is a member of.

--- api/v1/model/user.go
+++ api/v1/model/user.go
@@ -10,12 +10,13 @@ import (
 // User.go line 42
 // User example
 type User struct {
-	ID        uuid.UUID `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Name      string `json:"name"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  string `json:"password"`
+	ID            uuid.UUID `gorm:"primarykey"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Name          string         `json:"name"`
+	Email         string         `json:"email" gorm:"unique"`
+	Password      string         `json:"password"`
+	Organizations []Organization `json:"orgs,omitempty" gorm:"many2many:organization_user"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) error {
