2be06260fd7ebfb79cbf514cd122574f8d844be3
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-05-17 09:09:49 +0200

    Add cloud config to internal cloud types
    In this state it is a copy of the openstack config. As support for more
    cloud services are added this type will evolve.

--- internal/types/types_cloud.go
+++ internal/types/types_cloud.go
@@ -3,6 +3,17 @@ package types
 import "bitbucket.org/sudosweden/dockyards-backend/api/v1/model"
 
 type CloudService interface {
-	PrepareEnvironment(*model.Cluster, *model.NodePoolOptions) error
-	CleanEnvironment() error
+	PrepareEnvironment(*model.Cluster, *model.NodePoolOptions) (*CloudConfig, error)
+	CleanEnvironment(*CloudConfig) error
+}
+
+type CloudConfig struct {
+	AuthURL                     string
+	ApplicationCredentialID     string
+	ApplicationCredentialSecret string
+	FlavorID                    string
+	ImageID                     string
+	KeypairName                 string
+	NetID                       string
+	PrivateKeyFile              string
 }
