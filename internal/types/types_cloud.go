648cff86a8f149b1ade5a3f3e4f7500506728ae5
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-05-24 08:39:12 +0200

    Add organization as argument to prepare environment in cloud types
    Needed by the new organization support which uses the organization data
    to fetch cloud related resources.

--- internal/types/types_cloud.go
+++ internal/types/types_cloud.go
@@ -3,7 +3,7 @@ package types
 import "bitbucket.org/sudosweden/dockyards-backend/api/v1/model"
 
 type CloudService interface {
-	PrepareEnvironment(*model.Cluster, *model.NodePoolOptions) (*CloudConfig, error)
+	PrepareEnvironment(*model.Organization, *model.Cluster, *model.NodePoolOptions) (*CloudConfig, error)
 	CleanEnvironment(*CloudConfig) error
 	CreateOrganization(*model.Organization) (string, error)
 	GetOrganization(*model.Organization) (string, error)
