74ffaf2990e7c519784e688a66b588ad2f05cd07
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-05-30 14:09:17 +0200

    Add helm related fields to app model
    Adds the necessary fields to be able to specify repository and release
    for a helm chart.
    
    The helm values has to be treated a bit special. In order to support
    values that will be unique per chart a map of strings to any is used.
    
    In this initial version the helm values are just stored, even though the
    input might be invalid.

--- api/v1/model/model_app.go
+++ api/v1/model/model_app.go
@@ -5,13 +5,20 @@ import (
 	"gorm.io/gorm"
 )
 
+type HelmValues map[string]any
+
 type App struct {
-	ID             uuid.UUID `json:"id"`
-	Name           string    `json:"name"`
-	ContainerImage string    `json:"container_image"`
-	Organization   string    `json:"organization"`
-	Cluster        string    `json:"cluster"`
-	Port           int       `json:"port"`
+	ID             uuid.UUID  `json:"id"`
+	Name           string     `json:"name"`
+	ContainerImage string     `json:"container_image"`
+	Organization   string     `json:"organization"`
+	Cluster        string     `json:"cluster"`
+	Port           int        `json:"port"`
+	HelmChart      string     `json:"helm_chart,omitempty"`
+	HelmRepository string     `json:"helm_repository,omitempty"`
+	HelmVersion    string     `json:"helm_version,omitempty"`
+	HelmValues     HelmValues `json:"helm_values,omitempty"`
+	Namespace      string     `json:"namespace,omitempty"`
 }
 
 func (a *App) BeforeCreate(tx *gorm.DB) error {
