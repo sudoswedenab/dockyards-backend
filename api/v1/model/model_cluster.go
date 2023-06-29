c2684843c6c88fa0a15b200ca7e039452af6d31e
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-06-29 20:38:49 +0200

    Add node pools to cluster model

--- api/v1/model/model_cluster.go
+++ api/v1/model/model_cluster.go
@@ -3,10 +3,11 @@ package model
 import "time"
 
 type Cluster struct {
-	Name         string    `json:"name"`
-	ID           string    `json:"id"`
-	State        string    `json:"state"`
-	NodeCount    int       `json:"node_count"`
-	CreatedAt    time.Time `json:"created_at"`
-	Organization string    `json:"org,omitempty"`
+	Name         string     `json:"name"`
+	ID           string     `json:"id"`
+	State        string     `json:"state"`
+	NodeCount    int        `json:"node_count"`
+	CreatedAt    time.Time  `json:"created_at"`
+	Organization string     `json:"org,omitempty"`
+	NodePools    []NodePool `json:"node_pools,omitempty"`
 }
