7cf7632d1275c6f533b382e3f37ef70a6c36d389
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-07-03 12:55:53 +0200

    Add check for valid name during user signup

--- api/v1/handlers/signup.go
+++ api/v1/handlers/signup.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"bitbucket.org/sudosweden/dockyards-backend/api/v1/model"
+	"bitbucket.org/sudosweden/dockyards-backend/internal/names"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -16,6 +17,19 @@ func (h *handler) Signup(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to read Body",
 		})
+		return
+	}
+
+	details, validName := names.IsValidName(body.Name)
+	if !validName {
+		h.logger.Error("invalid name in signup request", "name", body.Name, "details", details)
+
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error":   "singnup name is not valid",
+			"name":    body.Name,
+			"details": details,
+		})
+
 		return
 	}
 
@@ -38,6 +52,8 @@ func (h *handler) Signup(c *gin.Context) {
 	result := h.db.Create(&user)
 
 	if result.Error != nil {
+		h.logger.Error("error creating user in database", "err", err)
+
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "failed to create User",
 		})
