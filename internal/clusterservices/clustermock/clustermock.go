0203ade07ff67f5a2c3d5145fa37af99f68672c3
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-16 15:58:08 +0200

    Move cluster service mock to dedicated package
    Currently the cluster service mock exists as a subpackage of the rancher
    cluster service.

--- internal/clusterservices/clustermock/clustermock.go
+++ internal/clusterservices/clustermock/clustermock.go
@@ -1,11 +1,11 @@
-package mock
+package clustermock
 
 import (
 	"bitbucket.org/sudosweden/dockyards-backend/api/v1/model"
 	"bitbucket.org/sudosweden/dockyards-backend/internal/types"
 )
 
-type MockRancherHelper struct {
+type MockClusterService struct {
 	MockCreateCluster  func(*model.Organization, *model.ClusterOptions) (*model.Cluster, error)
 	MockCreateNodePool func(*model.Organization, *model.Cluster, *model.NodePoolOptions) (*model.NodePool, error)
 	MockGetAllClusters func() (*[]model.Cluster, error)
@@ -14,36 +14,36 @@ type MockRancherHelper struct {
 	MockGetCluster     func(string) (*model.Cluster, error)
 }
 
-func (h *MockRancherHelper) CreateCluster(o *model.Organization, c *model.ClusterOptions) (*model.Cluster, error) {
+func (h *MockClusterService) CreateCluster(o *model.Organization, c *model.ClusterOptions) (*model.Cluster, error) {
 	return h.MockCreateCluster(o, c)
 }
 
-func (h *MockRancherHelper) CreateNodePool(org *model.Organization, c *model.Cluster, o *model.NodePoolOptions) (*model.NodePool, error) {
+func (h *MockClusterService) CreateNodePool(org *model.Organization, c *model.Cluster, o *model.NodePoolOptions) (*model.NodePool, error) {
 	return h.MockCreateNodePool(org, c, o)
 }
 
-func (h *MockRancherHelper) GetAllClusters() (*[]model.Cluster, error) {
+func (h *MockClusterService) GetAllClusters() (*[]model.Cluster, error) {
 	return h.MockGetAllClusters()
 }
 
-func (h *MockRancherHelper) DeleteCluster(o *model.Organization, c *model.Cluster) error {
+func (h *MockClusterService) DeleteCluster(o *model.Organization, c *model.Cluster) error {
 	return h.MockDeleteCluster(o, c)
 }
 
-func (h *MockRancherHelper) GetSupportedVersions() []string {
+func (h *MockClusterService) GetSupportedVersions() []string {
 	return []string{}
 }
 
-func (h *MockRancherHelper) GetKubeConfig(c *model.Cluster) (string, error) {
+func (h *MockClusterService) GetKubeConfig(c *model.Cluster) (string, error) {
 	return h.MockGetKubeConfig(c)
 }
 
-func (h *MockRancherHelper) DeleteGarbage() {
+func (h *MockClusterService) DeleteGarbage() {
 	return
 }
 
-func (h *MockRancherHelper) GetCluster(s string) (*model.Cluster, error) {
+func (h *MockClusterService) GetCluster(s string) (*model.Cluster, error) {
 	return h.MockGetCluster(s)
 }
 
-var _ types.ClusterService = &MockRancherHelper{}
+var _ types.ClusterService = &MockClusterService{}
