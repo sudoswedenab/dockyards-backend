e828ba49f5c256dc7ac359f75376b35727d8489b
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-16 12:45:21 +0200

    Add model for overviews
    Used for generating an overview of all resources that exists in
    dockyards.

---
+++ api/v1/model/model_overview.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"github.com/google/uuid"
+)
+
+type UserOverview struct {
+	ID    uuid.UUID `json:"id"`
+	Email string    `json:"email"`
+}
+
+type AppOverview struct {
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+}
+
+type ClusterOverview struct {
+	ID   string        `json:"id"`
+	Name string        `json:"name"`
+	Apps []AppOverview `json:"apps"`
+}
+
+type OrganizationOverview struct {
+	ID       uuid.UUID         `json:"id"`
+	Name     string            `json:"name"`
+	Clusters []ClusterOverview `json:"clusters"`
+	Users    []UserOverview    `json:"users"`
+}
+
+type Overview struct {
+	Organizations []OrganizationOverview `json:"organizations"`
+}
