d0e9c2599bcd0dc8788863fcc75c12eb7cd511b0
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-19 21:47:46 +0200

    Add compute resource fields to node pool model
    This adds CPU, RAM and disk resource fields to the node pool model to
    show resources for node pools when fetching full cluster info.

--- api/v1/model/model_node_pool.go
+++ api/v1/model/model_node_pool.go
@@ -7,4 +7,7 @@ type NodePool struct {
 	LoadBalancer               bool   `json:"load_balancer"`
 	Quantity                   int    `json:"quantity"`
 	ControlPlaneComponentsOnly bool   `json:"control_plane_components_only"`
+	CPUCount                   int    `json:"cpu_count"`
+	RAMSizeMB                  int    `json:"ram_size_mb"`
+	DiskSizeGB                 int    `json:"disk_size_gb"`
 }
