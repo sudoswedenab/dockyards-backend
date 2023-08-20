7936249e5969641f5e2e5887889b7761e350bd12
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-20 14:59:28 +0200

    Add basic cloudservice mock
    In this initial version only the get flavor node pool method is
    implemented.

---
+++ internal/cloudservices/cloudmock/cloudmock.go
@@ -0,0 +1,42 @@
+package cloudmock
+
+import (
+	"errors"
+
+	"bitbucket.org/sudosweden/dockyards-backend/api/v1/model"
+	"bitbucket.org/sudosweden/dockyards-backend/internal/types"
+)
+
+type MockCloudService struct {
+	types.CloudService
+	flavors map[string]*model.NodePool
+}
+
+var _ types.CloudService = &MockCloudService{}
+
+type MockOption func(*MockCloudService)
+
+func (s *MockCloudService) GetFlavorNodePool(flavorID string) (*model.NodePool, error) {
+	nodePool, hasNodePool := s.flavors[flavorID]
+	if !hasNodePool {
+		return nil, errors.New("not such flavor")
+
+	}
+	return nodePool, nil
+}
+
+func WithFlavors(flavors map[string]*model.NodePool) MockOption {
+	return func(s *MockCloudService) {
+		s.flavors = flavors
+	}
+}
+
+func NewMockCloudService(mockCloudOptions ...MockOption) *MockCloudService {
+	s := MockCloudService{}
+
+	for _, mockCloudOption := range mockCloudOptions {
+		mockCloudOption(&s)
+	}
+
+	return &s
+}
