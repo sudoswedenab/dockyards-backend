198e2ba02fc58219578f6983c81c1129ec74e967
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-22 08:45:57 +0200

    Add unit tests for register routes in handlers
    To ensure that any prefix conflicts are caught during testing.

---
+++ api/v1/handlers/handlers_test.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestRegisterRoutes(t *testing.T) {
+	tt := []struct {
+		name     string
+		expected error
+	}{
+		{
+			name: "test empty",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			r := gin.New()
+			db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+			if err != nil {
+				t.Fatalf("unexpected error creating db: %s", err)
+			}
+			logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError + 1}))
+			tokens := WithJWTAccessTokens("access123", "refresh123")
+			actual := RegisterRoutes(r, db, logger, tokens)
+			if actual != tc.expected {
+				t.Errorf("expected error %s, got %s", tc.expected, actual)
+			}
+		})
+	}
+}
