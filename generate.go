537e8d818c170452f0e222b92adeaa9202ce9f87
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-25 17:31:50 +0200

    Generate v1alpha1 custom resource definition

--- generate.go
+++ generate.go
@@ -4,3 +4,5 @@ package generate
 
 //go:generate oapi-codegen -config haxx/sudo/types.cfg.yaml haxx/sudo/spec.yaml
 //go:generate oapi-codegen -config haxx/sudo/server.cfg.yaml haxx/sudo/spec.yaml
+
+//go:generate controller-gen crd object:headerFile="haxx/header.txt" paths="./pkg/..."
