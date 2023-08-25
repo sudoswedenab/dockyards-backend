a142a88b277eb530bc6b79f7b10508565a8ad487
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-25 17:52:42 +0200

    Add configuration for generating sudo api

---
+++ generate.go
@@ -0,0 +1,6 @@
+package generate
+
+//go:generate oapi-codegen -config haxx/v1/types.cfg.yaml haxx/v1/spec.yaml
+
+//go:generate oapi-codegen -config haxx/sudo/types.cfg.yaml haxx/sudo/spec.yaml
+//go:generate oapi-codegen -config haxx/sudo/server.cfg.yaml haxx/sudo/spec.yaml
