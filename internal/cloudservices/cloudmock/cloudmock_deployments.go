ad91573172651e6b9fba7bc5c9df6b68278a7903
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-08-30 19:36:15 +0200

    Fix id in cluster deployments in clustermock

--- internal/cloudservices/cloudmock/cloudmock_deployments.go
+++ internal/cloudservices/cloudmock/cloudmock_deployments.go
@@ -7,7 +7,10 @@ import (
 func (s *MockCloudService) GetClusterDeployments(organization *v1.Organization, cluster *v1.Cluster) (*[]v1.Deployment, error) {
 	deployments := []v1.Deployment{}
 	for key := range s.deployments {
-		deployments = append(deployments, *s.deployments[key])
+		deployment := *s.deployments[key]
+		deployment.ClusterID = cluster.ID
+
+		deployments = append(deployments, deployment)
 	}
 
 	return &deployments, nil
