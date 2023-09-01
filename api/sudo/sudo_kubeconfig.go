10e1a54550b6cb300ca269348f939c067cefb268
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-09-01 09:38:03 +0200

    Add get kubeconfig implementatione to sudo api

--- api/sudo/sudo_kubeconfig.go
+++ api/sudo/sudo_kubeconfig.go
@@ -5,5 +5,13 @@ import (
 )
 
 func (a *sudoAPI) GetKubeconfig(ctx context.Context, req GetKubeconfigRequestObject) (GetKubeconfigResponseObject, error) {
-	return GetKubeconfig500Response{}, nil
+	kubeconfig, err := a.clusterService.GetKubeconfig(req.ClusterID, 0)
+	if err != nil {
+		a.logger.Error("error getting kubeconfig", "err", err)
+
+		return GetKubeconfig500Response{}, nil
+	}
+
+	return GetKubeconfig200JSONResponse(kubeconfig), nil
+
 }
