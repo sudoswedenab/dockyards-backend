bfb2fea49ec1937adca3b975e845cb78620f9fdd
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-09-21 20:29:28 +0200

    Add v1alpha1 organization type

---
+++ pkg/api/v1alpha1/organization_types.go
@@ -0,0 +1,37 @@
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	OrganizationKind = "Organization"
+)
+
+type OrganizationSpec struct {
+	DisplayName string                   `json:"displayName,omitempty"`
+	MemberRefs  []corev1.ObjectReference `json:"memberRefs"`
+	BillingRef  *corev1.ObjectReference  `json:"billingRef,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+type Organization struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec OrganizationSpec `json:"spec,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type OrganizationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Organization `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Organization{}, &OrganizationList{})
+}
