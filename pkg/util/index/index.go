6a08d56ebd429d144cfffc5dc2d3694502b71a66
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-09-25 09:08:50 +0200

    Add util package for kubernetes field indexers
    Adds indexers for email and uid on v1alpha1 user resources.

---
+++ pkg/util/index/index.go
@@ -0,0 +1,18 @@
+package index
+
+import (
+	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func EmailIndexer(object client.Object) []string {
+	return []string{
+		object.(*v1alpha1.User).Spec.Email,
+	}
+}
+
+func UIDIndexer(object client.Object) []string {
+	return []string{
+		string(object.(*v1alpha1.User).UID),
+	}
+}
