94e8cfc2fc153efa0c2f8fa52b08643559ab7067
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-09-25 21:19:11 +0200

    Add v1alpha1 kustomize deployment type

---
+++ pkg/api/v1alpha1/kustomizedeployment_types.go
@@ -0,0 +1,39 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	KustomizeDeploymentKind = "KustomizeDeployment"
+)
+
+type KustomizeDeploymentSpec struct {
+	Kustomize map[string][]byte `json:"kustomize"`
+}
+
+type KustomizeDeploymentStatus struct {
+	RepositoryURL string `json:"repositoryURL,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+type KustomizeDeployment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KustomizeDeploymentSpec   `json:"spec,omitempty"`
+	Status KustomizeDeploymentStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type KustomizeDeploymentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []KustomizeDeployment `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&KustomizeDeployment{}, &KustomizeDeploymentList{})
+}
