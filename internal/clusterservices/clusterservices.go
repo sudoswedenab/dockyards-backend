39f0eeffd37d96ac68ad084bfa2113f6375e7476
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-09-26 17:18:39 +0200

    Update methods to use v1alpha1 organization in cluster service

--- internal/clusterservices/clusterservices.go
+++ internal/clusterservices/clusterservices.go
@@ -10,14 +10,14 @@ import (
 
 type ClusterService interface {
 	CreateCluster(*v1alpha1.Organization, *v1.ClusterOptions) (*v1.Cluster, error)
-	CreateNodePool(*v1.Organization, *v1.Cluster, *v1.NodePoolOptions) (*v1.NodePool, error)
+	CreateNodePool(*v1alpha1.Organization, *v1.Cluster, *v1.NodePoolOptions) (*v1.NodePool, error)
 	GetAllClusters() (*[]v1.Cluster, error)
-	DeleteCluster(*v1.Organization, *v1.Cluster) error
+	DeleteCluster(*v1alpha1.Organization, *v1.Cluster) error
 	GetSupportedVersions() ([]string, error)
 	DeleteGarbage()
 	GetCluster(string) (*v1.Cluster, error)
 	CollectMetrics() error
 	GetNodePool(string) (*v1.NodePool, error)
 	GetKubeconfig(string, time.Duration) (*clientcmdv1.Config, error)
-	DeleteNodePool(*v1.Organization, string) error
+	DeleteNodePool(*v1alpha1.Organization, string) error
 }
