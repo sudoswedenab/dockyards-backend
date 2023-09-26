4cd8cca69cbb0276c0b07ebabe26e0814fcdc996
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-09-26 16:25:06 +0200

    Remove organization methods from cloud service interface
    Superseded by v1alpha1 organization types.

--- internal/cloudservices/cloudservices.go
+++ internal/cloudservices/cloudservices.go
@@ -7,12 +7,9 @@ import (
 type CloudService interface {
 	PrepareEnvironment(*v1.Organization, *v1.Cluster, *v1.NodePoolOptions) (*CloudConfig, error)
 	CleanEnvironment(*v1.Organization, *CloudConfig) error
-	CreateOrganization(*v1.Organization) (string, error)
-	GetOrganization(*v1.Organization) (string, error)
 	GetClusterDeployments(*v1.Organization, *v1.Cluster) (*[]v1.Deployment, error)
 	DeleteGarbage()
 	GetFlavorNodePool(string) (*v1.NodePool, error)
-	DeleteOrganization(*v1.Organization) error
 }
 
 type CloudConfig struct {
