8851a53337b789fd4f5bc55a9a43539389a07836
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-09-26 17:17:27 +0200

    Update methods to use v1alpha1 organization in cloud service

--- internal/cloudservices/cloudservices.go
+++ internal/cloudservices/cloudservices.go
@@ -2,12 +2,13 @@ package cloudservices
 
 import (
 	"bitbucket.org/sudosweden/dockyards-backend/api/v1"
+	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha1"
 )
 
 type CloudService interface {
-	PrepareEnvironment(*v1.Organization, *v1.Cluster, *v1.NodePoolOptions) (*CloudConfig, error)
-	CleanEnvironment(*v1.Organization, *CloudConfig) error
-	GetClusterDeployments(*v1.Organization, *v1.Cluster) (*[]v1.Deployment, error)
+	PrepareEnvironment(*v1alpha1.Organization, *v1.Cluster, *v1.NodePoolOptions) (*CloudConfig, error)
+	CleanEnvironment(*v1alpha1.Organization, *CloudConfig) error
+	GetClusterDeployments(*v1alpha1.Organization, *v1.Cluster) (*[]v1.Deployment, error)
 	DeleteGarbage()
 	GetFlavorNodePool(string) (*v1.NodePool, error)
 }
