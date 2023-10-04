ea309dff698de532f9ac686ef03a97846a5d1a5d
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-10-04 14:00:55 +0200

    Add cloudreference to v1alpha1 tytes
    Using the object reference type from core v1 is deprecated.

---
+++ pkg/api/v1alpha1/reference_types.go
@@ -0,0 +1,8 @@
+package v1alpha1
+
+type CloudReference struct {
+	APIVersion string `json:"apiVersion,omitempty"`
+	Kind       string `json:"kind,omitempty"`
+	Name       string `json:"name"`
+	SecretRef  string `json:"secretRef,omitempty"`
+}
