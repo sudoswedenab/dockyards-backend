24b365131d6163a1fed445291cb25300f070fba5
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-10-06 18:31:31 +0200

    Update get organizations to use matching fields in handlers

--- api/v1/handlers/handlers_orgs.go
+++ api/v1/handlers/handlers_orgs.go
@@ -7,6 +7,7 @@ import (
 	"bitbucket.org/sudosweden/dockyards-backend/api/v1"
 	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha1"
 	"github.com/gin-gonic/gin"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func (h *handler) GetOrgs(c *gin.Context) {
@@ -15,12 +16,17 @@ func (h *handler) GetOrgs(c *gin.Context) {
 	subject, err := h.getSubjectFromContext(c)
 	if err != nil {
 		h.logger.Debug("error fetching user from context", "err", err)
+
 		c.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
 
+	matchingFields := client.MatchingFields{
+		"spec.memberRefs": subject,
+	}
+
 	var organizationList v1alpha1.OrganizationList
-	err = h.controllerClient.List(ctx, &organizationList)
+	err = h.controllerClient.List(ctx, &organizationList, matchingFields)
 	if err != nil {
 		h.logger.Error("error listing organizations in kubernetes", "err", err)
 
@@ -30,15 +36,15 @@ func (h *handler) GetOrgs(c *gin.Context) {
 
 	var v1Organizations []v1.Organization
 	for _, organization := range organizationList.Items {
-		if h.isMember(subject, &organization) {
-			v1Organization := v1.Organization{
-				ID:   string(organization.UID),
-				Name: organization.Name,
-			}
-
-			v1Organizations = append(v1Organizations, v1Organization)
+		v1Organization := v1.Organization{
+			ID:   string(organization.UID),
+			Name: organization.Name,
 		}
+
+		v1Organizations = append(v1Organizations, v1Organization)
 	}
+
+	h.logger.Debug("organizations", "v1", v1Organizations)
 
 	c.JSON(http.StatusOK, v1Organizations)
 }
--- api/v1/handlers/handlers_orgs_test.go
+++ api/v1/handlers/handlers_orgs_test.go
@@ -13,6 +13,7 @@ import (
 
 	"bitbucket.org/sudosweden/dockyards-backend/api/v1"
 	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha1"
+	"bitbucket.org/sudosweden/dockyards-backend/pkg/util/index"
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-cmp/cmp"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -58,15 +59,137 @@ func TestGetOrgs(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "test multiple organizations",
+			lists: []client.ObjectList{
+				&v1alpha1.OrganizationList{
+					Items: []v1alpha1.Organization{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "test1",
+								UID:  "58c282c0-6a68-4ec8-9032-83d33f259bbe",
+							},
+							Spec: v1alpha1.OrganizationSpec{
+								MemberRefs: []v1alpha1.UserReference{
+									{
+										Name: "user1",
+										UID:  "2ca9e8a0-7b43-455d-867e-ed8bec4addfb",
+									},
+									{
+										Name: "user2",
+										UID:  "5cf0ed84-82f4-43fe-a3fb-b91f2ec7f0b1",
+									},
+								},
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "test2",
+								UID:  "d327da4c-f8fe-4f85-93a1-258b729a40d2",
+							},
+							Spec: v1alpha1.OrganizationSpec{
+								MemberRefs: []v1alpha1.UserReference{
+									{
+										Name: "user2",
+										UID:  "5cf0ed84-82f4-43fe-a3fb-b91f2ec7f0b1",
+									},
+								},
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "test3",
+								UID:  "5c13be53-fecd-467d-9546-d8ba3bb68103",
+							},
+							Spec: v1alpha1.OrganizationSpec{
+								MemberRefs: []v1alpha1.UserReference{
+									{
+										Name: "user1",
+										UID:  "2ca9e8a0-7b43-455d-867e-ed8bec4addfb",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			sub: "2ca9e8a0-7b43-455d-867e-ed8bec4addfb",
+			expected: []v1.Organization{
+				{
+					ID:   "58c282c0-6a68-4ec8-9032-83d33f259bbe",
+					Name: "test1",
+				},
+				{
+					ID:   "5c13be53-fecd-467d-9546-d8ba3bb68103",
+					Name: "test3",
+				},
+			},
+		},
+		{
+			name: "test subject without organizations",
+			lists: []client.ObjectList{
+				&v1alpha1.OrganizationList{
+					Items: []v1alpha1.Organization{
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "test1",
+								UID:  "57236ef2-304c-4fa7-9aa7-e8019dfa3070",
+							},
+							Spec: v1alpha1.OrganizationSpec{
+								MemberRefs: []v1alpha1.UserReference{
+									{
+										Name: "user1",
+										UID:  "92770876-ae7f-493f-b3f8-7d9f0a45b656",
+									},
+									{
+										Name: "user2",
+										UID:  "29625697-69c7-4142-92dc-dccccfb5b824",
+									},
+								},
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "test2",
+								UID:  "d327da4c-f8fe-4f85-93a1-258b729a40d2",
+							},
+							Spec: v1alpha1.OrganizationSpec{
+								MemberRefs: []v1alpha1.UserReference{
+									{
+										Name: "user3",
+										UID:  "df8ab98f-7866-4f4d-a9a6-7426879b7032",
+									},
+								},
+							},
+						},
+						{
+							ObjectMeta: metav1.ObjectMeta{
+								Name: "test3",
+								UID:  "5c13be53-fecd-467d-9546-d8ba3bb68103",
+							},
+							Spec: v1alpha1.OrganizationSpec{
+								MemberRefs: []v1alpha1.UserReference{
+									{
+										Name: "user4",
+										UID:  "d734d20f-e03e-44a8-89a5-8bd7f5d176d3",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			sub: "59862b3c-61de-4362-aeac-36366035a914",
+		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError + 1}))
+			logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError}))
 
 			scheme := scheme.Scheme
 			v1alpha1.AddToScheme(scheme)
-			fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithLists(tc.lists...).Build()
+			fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithLists(tc.lists...).WithIndex(&v1alpha1.Organization{}, "spec.memberRefs", index.MemberRefsIndexer).Build()
 
 			h := handler{
 				logger:           logger,
