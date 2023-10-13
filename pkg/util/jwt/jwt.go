1bafdf94e6a39fac1f6cf281d31d8d4652632ca9
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-10-13 12:32:06 +0200

    Add util for generating ecdsa keys for jwt

--- pkg/util/jwt/jwt.go
+++ pkg/util/jwt/jwt.go
@@ -2,8 +2,12 @@ package jwt
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
 	"errors"
 	"log/slog"
 
@@ -16,6 +20,9 @@ import (
 const (
 	defaultDockyardsNamespace = "dockyards"
 	defaultJWTSecretName      = "jwt-tokens"
+	AccessTokenPrivateKeyKey  = "accessTokenPrivateKey"
+	RefreshTokenPrivateKeyKey = "refreshTokenPrivateKey"
+	AccessTokenPublicKeyKey   = "accessTokenPublicKey"
 )
 
 func GetOrGenerateTokens(ctx context.Context, controllerClient client.Client, logger *slog.Logger) ([]byte, []byte, error) {
@@ -83,4 +90,133 @@ func GetOrGenerateTokens(ctx context.Context, controllerClient client.Client, lo
 	}
 
 	return accessToken, refreshToken, nil
+}
+
+func GetOrGenerateKeys(ctx context.Context, controllerClient client.Client, logger *slog.Logger) (*ecdsa.PrivateKey, *ecdsa.PrivateKey, error) {
+	objectKey := client.ObjectKey{
+		Name:      "dockyards-jwt",
+		Namespace: defaultDockyardsNamespace,
+	}
+
+	var secret corev1.Secret
+	err := controllerClient.Get(ctx, objectKey, &secret)
+	if client.IgnoreNotFound(err) != nil {
+		return nil, nil, err
+	}
+
+	if apierrors.IsNotFound(err) {
+		logger.Debug("generating keys due to missing secret", "name", objectKey.Name)
+
+		accessTokenPrivateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		if err != nil {
+			return nil, nil, err
+
+		}
+
+		b, err := x509.MarshalECPrivateKey(accessTokenPrivateKey)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		block := pem.Block{
+			Type:  "EC PRIVATE KEY",
+			Bytes: b,
+		}
+
+		accessTokenPrivateKeyPEM := pem.EncodeToMemory(&block)
+
+		refreshTokenPrivateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		b, err = x509.MarshalECPrivateKey(refreshTokenPrivateKey)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		block = pem.Block{
+			Type:  "EC PRIVATE KEY",
+			Bytes: b,
+		}
+
+		refreshTokenPrivateKeyPEM := pem.EncodeToMemory(&block)
+
+		secret = corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "dockyards-jwt",
+				Namespace: defaultDockyardsNamespace,
+			},
+			Data: map[string][]byte{
+				AccessTokenPrivateKeyKey:  accessTokenPrivateKeyPEM,
+				RefreshTokenPrivateKeyKey: refreshTokenPrivateKeyPEM,
+			},
+		}
+
+		err = controllerClient.Create(ctx, &secret)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		accessTokenPublicKey := accessTokenPrivateKey.PublicKey
+
+		b, err = x509.MarshalPKIXPublicKey(&accessTokenPublicKey)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		block = pem.Block{
+			Type:  "PUBLIC KEY",
+			Bytes: b,
+		}
+
+		accessTokenPublicKeyPEM := pem.EncodeToMemory(&block)
+
+		configMap := corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "dockyards-jwt",
+				Namespace: defaultDockyardsNamespace,
+			},
+			Data: map[string]string{
+				AccessTokenPublicKeyKey: string(accessTokenPublicKeyPEM),
+			},
+		}
+
+		err = controllerClient.Create(ctx, &configMap)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	accessTokenPrivateKeyPEM, hasPrivateKey := secret.Data[AccessTokenPrivateKeyKey]
+	if !hasPrivateKey {
+		return nil, nil, errors.New("secret has no private accesss key")
+	}
+
+	accessTokenPrivateKeyDER, _ := pem.Decode(accessTokenPrivateKeyPEM)
+	if accessTokenPrivateKeyDER == nil || accessTokenPrivateKeyDER.Type != "EC PRIVATE KEY" {
+		return nil, nil, errors.New("invalid access private key")
+	}
+
+	accessTokenPrivateKey, err := x509.ParseECPrivateKey(accessTokenPrivateKeyDER.Bytes)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	refreshTokenPrivateKeyPEM, hasPrivateKey := secret.Data[RefreshTokenPrivateKeyKey]
+	if !hasPrivateKey {
+		return nil, nil, errors.New("secret has no private refresh key")
+	}
+
+	refreshTokenPrivateKeyDER, _ := pem.Decode(refreshTokenPrivateKeyPEM)
+	if refreshTokenPrivateKeyDER == nil || refreshTokenPrivateKeyDER.Type != "EC PRIVATE KEY" {
+		return nil, nil, errors.New("invalid refresh private key")
+	}
+
+	refreshTokenPrivateKey, err := x509.ParseECPrivateKey(refreshTokenPrivateKeyDER.Bytes)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return accessTokenPrivateKey, refreshTokenPrivateKey, nil
 }
