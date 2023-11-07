45e5c9eafa3fa96767652bae88b60b769a76a505
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-11-07 19:28:24 +0100

    Add v1alpha1 release type

---
+++ pkg/api/v1alpha1/release_types.go
@@ -0,0 +1,40 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	ReleaseKind           = "Release"
+	ReleaseTypeKubernetes = "KubernetesReleases"
+)
+
+type ReleaseSpec struct {
+	Type string `json:"type"`
+}
+
+type ReleaseStatus struct {
+	Versions []string `json:"versions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+type Release struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ReleaseSpec   `json:"spec,omitempty"`
+	Status ReleaseStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type ReleaseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Release `json:"items,omitempty"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Release{}, &ReleaseList{})
+}
