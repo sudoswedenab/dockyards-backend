5c46dd008832f33b92919f623b08368e39991a02
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-11-22 11:24:07 +0100

    Update organization controller to reconcile delete
    Currently an organization has no backend finalizer causing organizations
    to be deleted before any clusters are fully removed.
    
    This adds adds a finalizer and a deletion reconciler to ensure that
    organizations are only deleted when there are no clusters left.

--- internal/controller/organization_controller.go
+++ internal/controller/organization_controller.go
@@ -4,11 +4,18 @@ import (
 	"context"
 	"log/slog"
 
+	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha1"
+	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha1/index"
 	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha2"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+const (
+	OrganizationFinalizer = "dockyards.io/backend-controller"
 )
 
 type OrganizationReconciler struct {
@@ -16,19 +23,40 @@ type OrganizationReconciler struct {
 	Logger *slog.Logger
 }
 
-// +kubebuilder:rbac:groups=dockyards.io,resources=organizations,verbs=get;list;watch
+// +kubebuilder:rbac:groups=dockyards.io,resources=organizations,verbs=get;list;patch;watch
 // +kubebuilder:rbac:groups=dockyards.io,resources=organizations/status,verbs=patch
 // +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;create
 
 func (r *OrganizationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := r.Logger.With("name", req.Name)
+
 	var organization v1alpha2.Organization
 	err := r.Get(ctx, req.NamespacedName, &organization)
-	if err != nil {
-		return ctrl.Result{}, client.IgnoreNotFound(err)
+	if client.IgnoreNotFound(err) != nil {
+		logger.Error("error getting organization", "err", err)
+
+		return ctrl.Result{}, err
+	}
+
+	if !organization.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(ctx, &organization)
+	}
+
+	if !controllerutil.ContainsFinalizer(&organization, clusterFinalizer) {
+		patch := client.MergeFrom(organization.DeepCopy())
+
+		controllerutil.AddFinalizer(&organization, OrganizationFinalizer)
+
+		err := r.Patch(ctx, &organization, patch)
+		if err != nil {
+			logger.Error("error adding finalizer", "err", err)
+
+			return ctrl.Result{}, err
+		}
 	}
 
 	if organization.Status.NamespaceRef == "" {
-		r.Logger.Info("organization has no namespace reference", "name", organization.Name)
+		logger.Info("organization has no namespace reference")
 
 		namespace := corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
@@ -46,31 +74,68 @@ func (r *OrganizationReconciler) Reconcile(ctx context.Context, req ctrl.Request
 
 		err := r.Create(ctx, &namespace)
 		if err != nil {
-			r.Logger.Error("error creating namespace", "err", err)
+			logger.Error("error creating namespace", "err", err)
 
 			return ctrl.Result{}, err
 		}
 
 		patch := client.MergeFrom(organization.DeepCopy())
+
 		organization.Status.NamespaceRef = namespace.Name
 
 		err = r.Status().Patch(ctx, &organization, patch)
 		if err != nil {
-			r.Logger.Error("error patching organization status", "err", err)
+			logger.Error("error patching organization status", "err", err)
 
 			return ctrl.Result{}, err
 		}
 
-		r.Logger.Debug("created namespace for organization", "name", namespace.Name)
+		logger.Debug("created namespace for organization")
 
 		return ctrl.Result{}, nil
 	}
 
-	r.Logger.Debug("nothing to reconcile for organization", "name", organization.Name)
+	return ctrl.Result{}, nil
+}
+
+func (r *OrganizationReconciler) reconcileDelete(ctx context.Context, organization *v1alpha2.Organization) (ctrl.Result, error) {
+	logger := r.Logger.With("name", organization.Name)
+
+	matchingFields := client.MatchingFields{
+		index.OwnerRefsIndexKey: string(organization.UID),
+	}
+
+	var clusterList v1alpha1.ClusterList
+	err := r.List(ctx, &clusterList, matchingFields)
+	if err != nil {
+		logger.Error("error listing clusters", "err", err)
+
+		return ctrl.Result{}, err
+	}
+
+	if len(clusterList.Items) != 0 {
+		logger.Info("ignoring deleted organization with clusters", "count", len(clusterList.Items))
+
+		return ctrl.Result{}, nil
+	}
+
+	patch := client.MergeFrom(organization.DeepCopy())
+
+	controllerutil.RemoveFinalizer(organization, clusterFinalizer)
+
+	err = r.Patch(ctx, organization, patch)
+	if err != nil {
+		logger.Error("error removing finalizer", "err", err)
+
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
 func (r *OrganizationReconciler) SetupWithManager(manager ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(manager).For(&v1alpha2.Organization{}).Complete(r)
+	return ctrl.NewControllerManagedBy(manager).
+		For(&v1alpha2.Organization{}).
+		Owns(&v1alpha1.Cluster{}).
+		Complete(r)
 }
