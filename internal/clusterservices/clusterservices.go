ee4bed344623973b902a81611c91f6c2459ed57a
Alexander Backlund <alexander.backlund@sudosweden.com>
2023-12-20 14:35:48 +0100

    Remove get kubeconfig from cluster service interface
    Functionality moved to other components.

--- internal/clusterservices/clusterservices.go
+++ internal/clusterservices/clusterservices.go
@@ -1,12 +1,9 @@
 package clusterservices
 
 import (
-	"time"
-
 	"bitbucket.org/sudosweden/dockyards-backend/api/v1"
 	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha1"
 	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha2"
-	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
 type ClusterService interface {
@@ -18,7 +15,6 @@ type ClusterService interface {
 	GetCluster(string) (*v1alpha1.ClusterStatus, error)
 	CollectMetrics() error
 	GetNodePool(string) (*v1alpha1.NodePoolStatus, error)
-	GetKubeconfig(string, time.Duration) (*clientcmdapi.Config, error)
 	DeleteNodePool(*v1alpha2.Organization, string) error
 	GetNodes(*v1alpha1.NodePool) (*v1alpha1.NodeList, error)
 	GetNode(string) (*v1alpha1.NodeStatus, error)
