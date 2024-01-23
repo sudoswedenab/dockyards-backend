826c0cd8715ce950f9b40c84bf5ae08b4d42d56b
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-01-23 09:01:34 +0100

    Update controller gen configuration to generate embedded metadata
    Currently the embedded metadata of node pool templates in cluster
    templates becomes an empty object. This results in any changes to the
    metadata to be discarded by the apiserver.
    
    This updates the configuration of controller gen to always copy the
    relevant metadata fields to the objects embedding them.

--- generate.go
+++ generate.go
@@ -2,4 +2,4 @@ package generate
 
 //go:generate oapi-codegen -config haxx/v1/types.cfg.yaml haxx/v1/spec.yaml
 
-//go:generate controller-gen crd object:headerFile="haxx/header.txt" rbac:roleName=dockyards-backend paths="./..."
+//go:generate controller-gen crd:generateEmbeddedObjectMeta=true object:headerFile="haxx/header.txt" rbac:roleName=dockyards-backend paths="./..."
