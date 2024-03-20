ec10777b827f4c6082e073ae14b76fd1f297dcf8
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-03-20 10:25:23 +0100

    Update node pool type to use ready as main condition
    To simplify condition management all dockyards types should use the same
    main condition type.

--- pkg/api/v1alpha1/nodepool_types.go
+++ pkg/api/v1alpha1/nodepool_types.go
@@ -34,7 +34,7 @@ type NodePoolStatus struct {
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:printcolumn:name="Provisioned",type=string,JSONPath=".status.conditions[?(@.type==\"Provisioned\")].status"
+// +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=".status.conditions[?(@.type==\"Ready\")].status"
 type NodePool struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
