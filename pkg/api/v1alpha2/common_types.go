3a1a67cab1f8faaf5f3e9f381505a840430b15a7
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-03-22 13:53:01 +0100

    Add common name labels for dockyards types
    To make is easier to use the same labels consistently across components.

---
+++ pkg/api/v1alpha2/common_types.go
@@ -0,0 +1,13 @@
+package v1alpha2
+
+const (
+	OrganizationNameLabel = "dockyards.io/organization-name"
+
+	ClusterNameLabel = "dockyards.io/cluster-name"
+
+	NodePoolNameLabel = "dockyards.io/node-pool-name"
+
+	NodeNameLabel = "dockyards.io/node-name"
+
+	DeploymentNameLabel = "dockyards.io/deploymennt-name"
+)
