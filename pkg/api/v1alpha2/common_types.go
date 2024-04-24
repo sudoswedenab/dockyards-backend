ed720216f8c4019250c62dfff5d95f3c195500aa
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-04-24 20:19:12 +0200

    Deprecate incorrectly prefixed label name constants
    For consistent behaviour with upstream behaviour all constants should
    begin with their category.

--- pkg/api/v1alpha2/common_types.go
+++ pkg/api/v1alpha2/common_types.go
@@ -1,13 +1,27 @@
 package v1alpha2
 
 const (
+	// Deprecated: use LabelOrganizationName
 	OrganizationNameLabel = "dockyards.io/organization-name"
 
+	// Deprecated: use LabelClusterName
 	ClusterNameLabel = "dockyards.io/cluster-name"
 
+	// Deprecated: use LabelNodePoolName
 	NodePoolNameLabel = "dockyards.io/node-pool-name"
 
+	// Deprecated: use LabelNodeName
 	NodeNameLabel = "dockyards.io/node-name"
 
+	// Deprecated: use LabelDeploymentName
 	DeploymentNameLabel = "dockyards.io/deploymennt-name"
+)
+
+const (
+	LabelOrganizationName = "dockyards.io/organization-name"
+	LabelClusterName      = "dockyards.io/cluster-name"
+	LabelNodePoolName     = "dockyards.io/node-pool-name"
+	LabelNodeName         = "dockyards.io/node-name"
+	LabelDeploymentName   = "dockyards.io/deployment-name"
+	LabelReleaseName      = "dockyards.io/release-name"
 )
