3e2c55cf40c8ddbc939cd454a110bf061250464c
Petr Savelyev <petr.savelyev@sudosweden.com>
2024-05-07 10:20:32 +0200

    Add verification request type to v1alpha2

---
+++ pkg/api/v1alpha2/verificationrequest_types.go
@@ -0,0 +1,37 @@
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type VerificationRequestSpec struct {
+    User string `json:"user"`
+    Code string `json:"code"`
+}
+
+type VerificationRequestStatus struct {
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:subresource:status
+type VerificationRequest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   VerificationRequestSpec   `json:"spec,omitempty"`
+	Status VerificationRequestStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type VerificationRequestList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []VerificationRequest `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&VerificationRequest{}, &VerificationRequestList{})
+}
