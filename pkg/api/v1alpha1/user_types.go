a52bb289c619e921302b7db47f3dfc6fb33b5fc1
Alex Vuletic <alexander.kierbalaj@sudosweden.com>
2024-10-03 10:04:16 +0200

    Add phone field to user type

--- pkg/api/v1alpha1/user_types.go
+++ pkg/api/v1alpha1/user_types.go
@@ -12,6 +12,7 @@ const (
 type UserSpec struct {
 	Email       string `json:"email"`
 	DisplayName string `json:"displayName,omitempty"`
+	Phone       string `json:"phone,omitempty"`
 
 	// +kubebuilder:validation:Pattern="^\\$2a\\$10\\$[\\.\\/A-Za-z0-9]{53}$"
 	Password string `json:"password"`
