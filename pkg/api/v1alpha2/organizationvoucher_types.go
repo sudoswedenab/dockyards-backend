dde9a62937f3f3a3f734460e8b14ffc44ab8267c
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-10-03 22:35:38 +0200

    Add organization voucher to v1alpha2 types

---
+++ pkg/api/v1alpha2/organizationvoucher_types.go
@@ -0,0 +1,44 @@
+package v1alpha2
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	OrganizationVoucherKind = "OrganizationVoucher"
+)
+
+type OrganizationVoucherSpec struct {
+	Code    string                       `json:"code"`
+	PoolRef *corev1.TypedObjectReference `json:"poolRef"`
+}
+
+type OrganizationVoucherStatus struct {
+	Redeemed bool `json:"redeemed,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="PoolReference",type=string,JSONPath=".spec.poolRef.name"
+// +kubebuilder:printcolumn:name="Code",type=string,JSONPath=".spec.code"
+// +kubebuilder:printcolumn:name="Redeemed",type=boolean,JSONPath=".status.redeemed"
+type OrganizationVoucher struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OrganizationVoucherSpec   `json:"spec,omitempty"`
+	Status OrganizationVoucherStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type OrganizationVoucherList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []OrganizationVoucher `json:"items,omitempty"`
+}
+
+func init() {
+	SchemeBuilder.Register(&OrganizationVoucher{}, &OrganizationVoucherList{})
+}
