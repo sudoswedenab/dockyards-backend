16e5c03afa0a4cb3473745480d15f32c7551efe8
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-10-05 17:23:25 +0200

    Add reason column to node pool v1alpha2 type

--- pkg/api/v1alpha2/nodepool_types.go
+++ pkg/api/v1alpha2/nodepool_types.go
@@ -44,6 +44,7 @@ type NodePoolStatus struct {
 // +kubebuilder:storageversion
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Ready",type=string,JSONPath=".status.conditions[?(@.type==\"Ready\")].status"
+// +kubebuilder:printcolumn:name="Reason",type=string,JSONPath=".status.conditions[?(@.type==\"Ready\")].reason"
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=".metadata.creationTimestamp"
 type NodePool struct {
 	metav1.TypeMeta   `json:",inline"`
