fdbdc11b38ce44f00137cac8d2f07fde820f0f32
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-10-24 08:40:10 +0200

    Move organization controller role access methods to separate package
    Currently only the controller can create the role and role bindings for
    organization member references. This causes issues when creating
    integratin tests can need to run without any controllers enabled.
    
    This moves all the methods for reconciling the role and role bindings to
    a new package that can be used by other parts of this component. The
    responsibility of actually reconciling the membership references still
    remains with the organization controller it just uses the new helper
    methods to do it.

---
+++ pkg/authorization/authorization.go
@@ -0,0 +1,423 @@
+package authorization
+
+import (
+	"context"
+
+	dockyardsv1 "bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha3"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+func ReconcileUserRoleAndBindings(ctx context.Context, c client.Client, organization *dockyardsv1.Organization) error {
+	logger := ctrl.LoggerFrom(ctx)
+
+	role := rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dockyards-user",
+			Namespace: organization.Status.NamespaceRef.Name,
+		},
+	}
+
+	operationResult, err := controllerutil.CreateOrPatch(ctx, c, &role, func() error {
+		role.OwnerReferences = []metav1.OwnerReference{
+			{
+				APIVersion: dockyardsv1.GroupVersion.String(),
+				Kind:       dockyardsv1.OrganizationKind,
+				Name:       organization.Name,
+				UID:        organization.UID,
+			},
+		}
+
+		if role.Labels == nil {
+			role.Labels = make(map[string]string)
+		}
+
+		role.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		role.Rules = []rbacv1.PolicyRule{
+			{
+				Verbs: []string{
+					"create",
+					"delete",
+					"patch",
+					"watch",
+				},
+				APIGroups: []string{
+					dockyardsv1.GroupVersion.Group,
+				},
+				Resources: []string{
+					"*",
+				},
+			},
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled role", "roleName", role.Name, "result", operationResult)
+	}
+
+	roleBinding := rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dockyards-user",
+			Namespace: organization.Status.NamespaceRef.Name,
+		},
+	}
+
+	operationResult, err = controllerutil.CreateOrPatch(ctx, c, &roleBinding, func() error {
+		if roleBinding.Labels == nil {
+			roleBinding.Labels = make(map[string]string)
+		}
+
+		roleBinding.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		roleBinding.RoleRef = rbacv1.RoleRef{
+			Kind: "Role",
+			Name: role.Name,
+		}
+
+		subjects := []rbacv1.Subject{}
+
+		for _, memberRef := range organization.Spec.MemberRefs {
+			if memberRef.Role == dockyardsv1.OrganizationMemberRoleReader {
+				continue
+			}
+
+			subjects = append(subjects, rbacv1.Subject{
+				APIGroup: rbacv1.GroupName,
+				Kind:     rbacv1.UserKind,
+				Name:     string(memberRef.UID),
+			})
+		}
+
+		roleBinding.Subjects = subjects
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled rolebinding", "roleBindingName", roleBinding.Name, "result", operationResult)
+	}
+
+	return nil
+}
+
+func ReconcileSuperUserClusterRoleAndBinding(ctx context.Context, c client.Client, organization *dockyardsv1.Organization) error {
+	logger := ctrl.LoggerFrom(ctx)
+
+	clusterRole := rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "dockyards-" + organization.Name + "-superuser",
+		},
+	}
+
+	operationResult, err := controllerutil.CreateOrPatch(ctx, c, &clusterRole, func() error {
+		clusterRole.OwnerReferences = []metav1.OwnerReference{
+			{
+				APIVersion: dockyardsv1.GroupVersion.String(),
+				Kind:       dockyardsv1.OrganizationKind,
+				Name:       organization.Name,
+				UID:        organization.UID,
+			},
+		}
+
+		if clusterRole.Labels == nil {
+			clusterRole.Labels = make(map[string]string)
+		}
+
+		clusterRole.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		clusterRole.Rules = []rbacv1.PolicyRule{
+			{
+				Verbs: []string{
+					"*",
+				},
+				APIGroups: []string{
+					dockyardsv1.GroupVersion.Group,
+				},
+				Resources: []string{
+					"organizations",
+				},
+				ResourceNames: []string{
+					organization.Name,
+				},
+			},
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled clusterrole", "clusterRoleName", clusterRole.Name, "result", operationResult)
+	}
+
+	clusterRoleBinding := rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "dockyards-" + organization.Name + "-superuser",
+		},
+	}
+
+	operationResult, err = controllerutil.CreateOrPatch(ctx, c, &clusterRoleBinding, func() error {
+		clusterRoleBinding.OwnerReferences = []metav1.OwnerReference{
+			{
+				APIVersion: dockyardsv1.GroupVersion.String(),
+				Kind:       dockyardsv1.OrganizationKind,
+				Name:       organization.Name,
+				UID:        organization.UID,
+			},
+		}
+
+		if clusterRoleBinding.Labels == nil {
+			clusterRoleBinding.Labels = make(map[string]string)
+		}
+
+		clusterRoleBinding.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		clusterRoleBinding.RoleRef = rbacv1.RoleRef{
+			Kind: "ClusterRole",
+			Name: clusterRole.Name,
+		}
+
+		subjects := []rbacv1.Subject{}
+
+		for _, memberRef := range organization.Spec.MemberRefs {
+			if memberRef.Role != dockyardsv1.OrganizationMemberRoleSuperUser {
+				continue
+			}
+
+			subjects = append(subjects, rbacv1.Subject{
+				APIGroup: rbacv1.GroupName,
+				Kind:     rbacv1.UserKind,
+				Name:     string(memberRef.UID),
+			})
+		}
+
+		clusterRoleBinding.Subjects = subjects
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled clusterrolebinding", "clusterRoleBindingName", clusterRoleBinding.Name, "result", operationResult)
+	}
+
+	return nil
+}
+
+func ReconcileReaderClusterRoleAndBinding(ctx context.Context, c client.Client, organization *dockyardsv1.Organization) error {
+	logger := ctrl.LoggerFrom(ctx)
+
+	clusterRole := rbacv1.ClusterRole{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "dockyards-" + organization.Name + "-reader",
+		},
+	}
+
+	operationResult, err := controllerutil.CreateOrPatch(ctx, c, &clusterRole, func() error {
+		clusterRole.OwnerReferences = []metav1.OwnerReference{
+			{
+				APIVersion: dockyardsv1.GroupVersion.String(),
+				Kind:       dockyardsv1.OrganizationKind,
+				Name:       organization.Name,
+				UID:        organization.UID,
+			},
+		}
+
+		if clusterRole.Labels == nil {
+			clusterRole.Labels = make(map[string]string)
+		}
+
+		clusterRole.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		clusterRole.Rules = []rbacv1.PolicyRule{
+			{
+				Verbs: []string{
+					"get",
+					"list",
+					"watch",
+				},
+				APIGroups: []string{
+					dockyardsv1.GroupVersion.Group,
+				},
+				Resources: []string{
+					"organizations",
+				},
+				ResourceNames: []string{
+					organization.Name,
+				},
+			},
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled clusterrole", "clusterRoleName", clusterRole.Name, "result", operationResult)
+	}
+
+	clusterRoleBinding := rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "dockyards-" + organization.Name + "-reader",
+		},
+	}
+
+	operationResult, err = controllerutil.CreateOrPatch(ctx, c, &clusterRoleBinding, func() error {
+		clusterRoleBinding.OwnerReferences = []metav1.OwnerReference{
+			{
+				APIVersion: dockyardsv1.GroupVersion.String(),
+				Kind:       dockyardsv1.OrganizationKind,
+				Name:       organization.Name,
+				UID:        organization.UID,
+			},
+		}
+
+		if clusterRoleBinding.Labels == nil {
+			clusterRoleBinding.Labels = make(map[string]string)
+		}
+
+		clusterRoleBinding.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		clusterRoleBinding.RoleRef = rbacv1.RoleRef{
+			Kind: "ClusterRole",
+			Name: clusterRole.Name,
+		}
+
+		subjects := make([]rbacv1.Subject, len(organization.Spec.MemberRefs))
+
+		for i, memberRef := range organization.Spec.MemberRefs {
+			subjects[i] = rbacv1.Subject{
+				APIGroup: rbacv1.GroupName,
+				Kind:     rbacv1.UserKind,
+				Name:     string(memberRef.UID),
+			}
+		}
+
+		clusterRoleBinding.Subjects = subjects
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled clusterrolebinding", "clusterRoleBindingName", clusterRoleBinding.Name, "result", operationResult)
+	}
+
+	return nil
+}
+
+func ReconcileReaderRoleAndBinding(ctx context.Context, c client.Client, organization *dockyardsv1.Organization) error {
+	logger := ctrl.LoggerFrom(ctx)
+
+	role := rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dockyards-reader",
+			Namespace: organization.Status.NamespaceRef.Name,
+		},
+	}
+
+	operationResult, err := controllerutil.CreateOrPatch(ctx, c, &role, func() error {
+		role.OwnerReferences = []metav1.OwnerReference{
+			{
+				APIVersion: dockyardsv1.GroupVersion.String(),
+				Kind:       dockyardsv1.OrganizationKind,
+				Name:       organization.Name,
+				UID:        organization.UID,
+			},
+		}
+
+		if role.Labels == nil {
+			role.Labels = make(map[string]string)
+		}
+
+		role.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		role.Rules = []rbacv1.PolicyRule{
+			{
+				Verbs: []string{
+					"get",
+					"list",
+					"watch",
+				},
+				APIGroups: []string{
+					dockyardsv1.GroupVersion.Group,
+				},
+				Resources: []string{
+					"*",
+				},
+			},
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled role", "roleName", role.Name, "result", operationResult)
+	}
+
+	roleBinding := rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "dockyards-reader",
+			Namespace: organization.Status.NamespaceRef.Name,
+		},
+	}
+
+	operationResult, err = controllerutil.CreateOrPatch(ctx, c, &roleBinding, func() error {
+		if roleBinding.Labels == nil {
+			roleBinding.Labels = make(map[string]string)
+		}
+
+		roleBinding.Labels[dockyardsv1.LabelOrganizationName] = organization.Name
+
+		roleBinding.RoleRef = rbacv1.RoleRef{
+			Kind: "Role",
+			Name: role.Name,
+		}
+
+		subjects := make([]rbacv1.Subject, len(organization.Spec.MemberRefs))
+
+		for i, memberRef := range organization.Spec.MemberRefs {
+			subjects[i] = rbacv1.Subject{
+				APIGroup: rbacv1.GroupName,
+				Kind:     rbacv1.UserKind,
+				Name:     string(memberRef.UID),
+			}
+		}
+
+		roleBinding.Subjects = subjects
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if operationResult != controllerutil.OperationResultNone {
+		logger.Info("reconciled rolebinding", "roleBindingName", roleBinding.Name, "result", operationResult)
+	}
+
+	return nil
+}
