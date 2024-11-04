6df0e6a6acef830acc6d76eed817ed0a1dce0681
Petr Savelyev <petr.savelyev@sudosweden.com>
2024-11-04 13:18:21 +0100

    Add ProviderID to verification request

--- pkg/api/v1alpha3/verificationrequest_types.go
+++ pkg/api/v1alpha3/verificationrequest_types.go
@@ -14,6 +14,7 @@ type VerificationRequestSpec struct {
 }
 
 type VerificationRequestStatus struct {
+	ProviderID string             `json:"providerID,omitempty"`
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
