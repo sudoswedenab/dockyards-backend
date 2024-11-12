a21a79322aeaa0988e748420fa738e0c50b0ad5a
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-11-12 16:24:53 +0100

    Add status subresource to v1alpha3 workload template type
    This adds a status subresource to the workload template type. Used to
    store the optional input definition of the workload template source.

--- pkg/api/v1alpha3/workloadtemplate_types.go
+++ pkg/api/v1alpha3/workloadtemplate_types.go
@@ -1,6 +1,7 @@
 package v1alpha3
 
 import (
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -19,14 +20,20 @@ type WorkloadTemplateSpec struct {
 	Type   WorkloadTemplateType `json:"type"`
 }
 
+type WorkloadTemplateStatus struct {
+	InputSchema *apiextensionsv1.JSON `json:"inputSchema,omitempty"`
+}
+
 // +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="Type",type=string,JSONPath=".spec.type"
 // +kubebuilder:printcolumn:name="Age",type=date,JSONPath=".metadata.creationTimestamp"
 type WorkloadTemplate struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec WorkloadTemplateSpec `json:"spec,omitempty"`
+	Spec   WorkloadTemplateSpec   `json:"spec,omitempty"`
+	Status WorkloadTemplateStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
