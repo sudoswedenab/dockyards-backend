039c87b398f8588433ff495d3d442852b4804503
Alexander Backlund <alexander.backlund@sudosweden.com>
2024-12-20 14:49:12 +0100

    Add generic helper for update cluster resource handlers

---
+++ internal/api/v1/handlers/update.go
@@ -0,0 +1,191 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"bitbucket.org/sudosweden/dockyards-api/pkg/types"
+	"bitbucket.org/sudosweden/dockyards-backend/internal/api/v1/middleware"
+	"bitbucket.org/sudosweden/dockyards-backend/pkg/api/apiutil"
+	dockyardsv1 "bitbucket.org/sudosweden/dockyards-backend/pkg/api/v1alpha3"
+	authorizationv1 "k8s.io/api/authorization/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type UpdateClusterResourceFunc[T any] func(context.Context, *dockyardsv1.Cluster, string, *T) error
+
+func UpdateClusterResource[T any](h *handler, resource string, f UpdateClusterResourceFunc[T]) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		logger := middleware.LoggerFrom(ctx).With("resource", resource)
+
+		organizationName := r.PathValue("organizationName")
+		if organizationName == "" {
+			w.WriteHeader(http.StatusBadRequest)
+
+			return
+		}
+
+		clusterName := r.PathValue("clusterName")
+		if clusterName == "" {
+			w.WriteHeader(http.StatusBadRequest)
+
+			return
+		}
+
+		resourceName := r.PathValue("resourceName")
+		if resourceName == "" {
+			w.WriteHeader(http.StatusBadRequest)
+
+			return
+		}
+
+		var organization dockyardsv1.Organization
+		err := h.Get(ctx, client.ObjectKey{Name: organizationName}, &organization)
+		if client.IgnoreNotFound(err) != nil {
+			logger.Error("error getting organization", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		if apierrors.IsNotFound(err) {
+			w.WriteHeader(http.StatusUnauthorized)
+
+			return
+		}
+
+		if organization.Status.NamespaceRef == nil {
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		subject, err := middleware.SubjectFrom(ctx)
+		if err != nil {
+			logger.Error("error getting subject from context", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		resourceAttributes := authorizationv1.ResourceAttributes{
+			Group:     dockyardsv1.GroupVersion.Group,
+			Namespace: organization.Status.NamespaceRef.Name,
+			Resource:  resource,
+			Verb:      "patch",
+		}
+
+		allowed, err := apiutil.IsSubjectAllowed(ctx, h.Client, subject, &resourceAttributes)
+		if err != nil {
+			logger.Error("error reviewing subject", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		if !allowed {
+			logger.Debug("subject is not allowed to patch resource", "subject", subject, "organization", organization.Name)
+			w.WriteHeader(http.StatusUnauthorized)
+
+			return
+		}
+
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Error("error reading request body", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		objectKey := client.ObjectKey{
+			Name:      clusterName,
+			Namespace: organization.Status.NamespaceRef.Name,
+		}
+
+		var cluster dockyardsv1.Cluster
+		err = h.Get(ctx, objectKey, &cluster)
+		if client.IgnoreNotFound(err) != nil {
+			logger.Error("error getting cluster", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		if apierrors.IsNotFound(err) {
+			w.WriteHeader(http.StatusNotFound)
+
+			return
+		}
+
+		var request T
+		err = json.Unmarshal(b, &request)
+		if err != nil {
+			logger.Error("error unmarshalling request", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		err = f(ctx, &cluster, resourceName, &request)
+		if apierrors.IsForbidden(err) {
+			w.WriteHeader(http.StatusForbidden)
+
+			return
+		}
+
+		if apierrors.IsInvalid(err) {
+			statusError, ok := err.(*apierrors.StatusError)
+			if !ok {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+
+				return
+			}
+
+			if statusError.ErrStatus.Details == nil {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+
+				return
+			}
+
+			var response types.UnprocessableEntityErrors
+
+			for _, cause := range statusError.ErrStatus.Details.Causes {
+				response.Errors = append(response.Errors, cause.Message)
+			}
+
+			b, err := json.Marshal(response)
+			if err != nil {
+				logger.Error("error marhalling response", "err", err)
+				w.WriteHeader(http.StatusInternalServerError)
+
+				return
+			}
+
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			_, err = w.Write(b)
+			if err != nil {
+				logger.Error("error writing response", "err", err)
+				w.WriteHeader(http.StatusInternalServerError)
+
+				return
+			}
+
+			return
+		}
+
+		if err != nil {
+			logger.Error("error updating resource", "err", err)
+			w.WriteHeader(http.StatusInternalServerError)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
