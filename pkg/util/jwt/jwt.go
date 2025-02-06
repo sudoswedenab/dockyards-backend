e561ada69086edb8ca82def8bf8dea6b43dd9e6f
Alexander Backlund <alexander.backlund@sudosweden.com>
2025-02-06 14:19:26 +0100

    Remove unused generate jwt tokens helper

--- pkg/util/jwt/jwt.go
+++ pkg/util/jwt/jwt.go
@@ -20,13 +20,10 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/x509"
-	"encoding/base64"
 	"encoding/pem"
 	"errors"
-	"log/slog"
 
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -37,78 +34,10 @@ import (
 
 const (
 	defaultDockyardsNamespace = "dockyards"
-	defaultJWTSecretName      = "jwt-tokens"
 	AccessTokenPrivateKeyKey  = "accessTokenPrivateKey"
 	RefreshTokenPrivateKeyKey = "refreshTokenPrivateKey"
 	AccessTokenPublicKeyKey   = "accessTokenPublicKey"
 )
-
-func GetOrGenerateTokens(ctx context.Context, controllerClient client.Client, logger *slog.Logger) ([]byte, []byte, error) {
-	empty := []byte{}
-
-	objectKey := client.ObjectKey{
-		Namespace: defaultDockyardsNamespace,
-		Name:      defaultJWTSecretName,
-	}
-
-	var secret corev1.Secret
-	err := controllerClient.Get(ctx, objectKey, &secret)
-	if client.IgnoreNotFound(err) != nil {
-		return empty, empty, err
-	}
-
-	if apierrors.IsNotFound(err) {
-		logger.Debug("generating private secrets")
-
-		b := make([]byte, 32)
-		_, err := rand.Read(b)
-		if err != nil {
-			return empty, empty, err
-		}
-		accessToken := base64.StdEncoding.EncodeToString(b)
-
-		logger.Debug("generated access token")
-
-		b = make([]byte, 32)
-		_, err = rand.Read(b)
-		if err != nil {
-			return empty, empty, err
-		}
-		refreshToken := base64.StdEncoding.EncodeToString(b)
-
-		logger.Debug("generated refresh token")
-
-		secret = corev1.Secret{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: defaultDockyardsNamespace,
-				Name:      defaultJWTSecretName,
-			},
-			StringData: map[string]string{
-				"accessToken":  accessToken,
-				"refreshToken": refreshToken,
-			},
-		}
-
-		err = controllerClient.Create(ctx, &secret)
-		if err != nil {
-			return empty, empty, err
-		}
-
-		logger.Debug("created jwt tokens secret in kubernetes", "uid", secret.UID)
-	}
-
-	accessToken, hasToken := secret.Data["accessToken"]
-	if !hasToken {
-		return empty, empty, errors.New("jwt tokens secret has no access token in data")
-	}
-
-	refreshToken, hasToken := secret.Data["refreshToken"]
-	if !hasToken {
-		return empty, empty, errors.New("jwt tokens secret has no refresh token in data")
-	}
-
-	return accessToken, refreshToken, nil
-}
 
 func GetOrGenerateKeys(ctx context.Context, controllerClient client.Client) (*ecdsa.PrivateKey, *ecdsa.PrivateKey, error) {
 	var (
