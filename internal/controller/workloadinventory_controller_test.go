08eafaee39a268f262e7b5d2507605dc4e4d8393
Alexander Backlund <alexander.backlund@sudosweden.com>
2025-05-28 08:50:02 +0200

    Fix workload inventory by selector tests not waiting for resources
    Currently the workload inventory by selector tests execute without
    waiting for created resources to exist. This causes random issues in
    slower execution environments.

--- internal/controller/workloadinventory_controller_test.go
+++ internal/controller/workloadinventory_controller_test.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Sudo Sweden AB
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package controller_test
 
 import (
@@ -110,6 +124,11 @@ func TestWorkloadInventory_BySelectorIndex(t *testing.T) {
 
 	for _, workloadInventory := range workloadInventoryList.Items {
 		err := c.Create(ctx, &workloadInventory)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = testingutil.RetryUntilFound(ctx, mgr.GetClient(), &workloadInventory)
 		if err != nil {
 			t.Fatal(err)
 		}
