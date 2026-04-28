134d8acdcc460600f1da0fe91895b561ad13e95e
Junior Rantila <junior.rantila@sudosweden.com>
2026-04-28 12:39:19 +0200

    Add validating webhooks for credentials

--- main.go
+++ main.go
@@ -120,6 +120,13 @@ func setupWebhooks(mgr ctrl.Manager, allowedDomains []string) error {
 		return err
 	}
 
+	err = (&webhooks.DockyardsCredential{
+		Client: mgr.GetClient(),
+	}).SetupWebhookWithManager(mgr)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
