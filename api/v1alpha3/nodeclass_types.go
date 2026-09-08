adfded56540f8fdef5b18a00fa73e514b472d427
Maxim Kravchenko <maxim.kravchenko@sudosweden.com>
2026-09-08 15:46:30 +0200

    feat: allow Dockyards Nodes to be run on specific K8s nodes via NodeClass
    This commit introduces a helper object NodeClass that is intended to
    describe certain Affinities/Selectors/Tolerations that will allow the
    users to spawn NodePool's nodes on specific K8s nodes.
    
    The idea is that the NodeClass should be an object that allows the
    administrators to conceal selection complexity from the end users; the
    end users should be able to get the names of NodeClasses to make
    scheduling decisions

---
+++ api/v1alpha3/nodeclass_types.go
@@ -0,0 +1,50 @@
+// Copyright 2024 Sudo Sweden AB
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha3
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	NodeClassKind = "NodeClass"
+)
+
+type NodeClassSpec struct {
+	NodeSelector map[string]string   `json:"nodeSelector,omitempty"`
+	Affinity     *corev1.Affinity    `json:"affinity,omitempty"`
+	Tolerations  []corev1.Toleration `json:"tolerations,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type NodeClass struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec NodeClassSpec `json:"spec,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type NodeClassList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []NodeClass `json:"items,omitempty"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NodeClass{}, &NodeClassList{})
+}
